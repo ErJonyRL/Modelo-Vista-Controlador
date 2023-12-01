923f700b14f7ad12cc62e6b9b330fa80523cb418
Administrador <Administrador@2DAM01>
2023-12-01 18:37:12 +0100

    correccion en controlador

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -2,12 +2,9 @@ package Controlador;
 
 public class Controlador {
 
-<<<<<<< HEAD
-=======
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
->>>>>>> branch 'master' of git@github.com:ErJonyRL/PCHelper.git
 }
